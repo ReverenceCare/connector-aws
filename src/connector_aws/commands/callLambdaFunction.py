a01f1e816833d7c4bab3b480cbe61803e493842b
ruze <ruze00@gmail.com>
2023-08-03 17:23:26 -0400

    streamline

--- src/connector_aws/commands/callLambdaFunction.py
+++ src/connector_aws/commands/callLambdaFunction.py
@@ -1,41 +1,24 @@
-"""QueryDynamoTable."""
-import simplejson as json
-from boto3.dynamodb.conditions import Key
-from connector_aws.auths.simpleAuth import SimpleAuth  # type: ignore
+"""callLambdaFunction."""
+import boto3
+import json
 
 
 class CallLambdaFunction:
-    """Return all records for a given partition key, and optionally a sort_key."""
+    """Call an RCP lambda function."""
 
-    def __init__(self, table_name: str, partition_key: str, sort_key: str = None):
+    def __init__(self, lambda_name: str):
         """
-        :param table_name: The name of hte Dynamo DB table to add information to.
-        :param partition_key: The value to search for in the partition key
-        :param sort_key: The value to search for in the sork_key (optional)
-        :return: Json Data structure containing the requested data.
+        :param lambda_name: The name of the lambda function to call
+        :return: Json Data structure containing the response from the lambda.
         """
-        self.table_name = table_name
-        self.partition_key = partition_key
-        self.sort_key = sort_key
+        self.lambda_name = lambda_name
 
     def execute(self, config, task_data):
         """Execute."""
-        dynamodb = SimpleAuth("dynamodb", config).get_resource()
-        table = dynamodb.Table(self.table_name)
-        partition_key_name = self.get_schema_key_name(table, "HASH")
-        sort_key_name = self.get_schema_key_name(table, "RANGE")
-        query = {partition_key_name: self.partition_key}
-        condition = Key(partition_key_name).eq(self.partition_key)
-        if self.sort_key:
-            condition = condition & Key(sort_key_name).eq(self.sort_key)
-        result = table.query(KeyConditionExpression=condition)
-        if "ResponseMetadata" in result:
-            del result["ResponseMetadata"]
-        result_str = json.dumps(result)
-        return dict(response=result_str, mimetype="application/json")
-
-    def get_schema_key_name(self, table, key_type: str):
-        """Get_schema_key_name."""
-        for item in table.key_schema:
-            if item["KeyType"] == key_type:
-                return item["AttributeName"]
+        client = boto3.client("lambda")
+        response = client.invoke(
+            FunctionName=self.lambda_name,
+            InvocationType="RequestResponse",
+            Payload=json.dumps(task_data),
+        )
+        return json.loads(response["Payload"].read())
